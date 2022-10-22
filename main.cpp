a99e160980c2c8b0972b439d535515119e5c1600
Sovember <michaelajohnsonresume@gmail.com>
2022-10-21 20:43:13 -0500

    First commit
    Hey

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+
+class node {
+
+    private: 
+
+    typedef int value_type;
+
+    public:
+
+    value_type data;
+    node* link;
+
+};
+
+int main() {
+
+
+}
